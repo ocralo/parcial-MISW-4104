dc4cda82c524cc65a3dc273ab4e4fbce454ff569
ocralo <ocralo@gmail.com>
2022-11-03 23:33:46 -0500

    feat: add new tests

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,23 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HomeComponent} from 'src/app/pages/home/home.component';
 
-import { NavbarComponent } from './navbar.component';
+import {NavbarComponent} from './navbar.component';
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+	let component: NavbarComponent;
+	let fixture: ComponentFixture<NavbarComponent>;
+	let element: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [NavbarComponent],
+			imports: [RouterTestingModule],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(NavbarComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('Navbar text', () => {
+		beforeEach(() => {
+			element = fixture.debugElement.query(By.css('.navbar-brand-text'));
+		});
+
+		it('should be containt El aroma mágico', () => {
+			expect(element.nativeElement.textContent.trim()).toBe('El aroma mágico');
+		});
+
+		it('should be containt El aroma mágico', () => {
+			expect(element.nativeElement.getAttribute('href')).toBe('/');
+		});
+	});
 });
