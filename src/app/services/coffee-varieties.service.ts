fdbe3dbb4acb13894d1f7e6068c66bdb34232df4
ocralo <ocralo@gmail.com>
2022-11-04 00:56:28 -0500

    feat: create service to get coffee varieties

--- src/app/services/coffee-varieties.service.ts
+++ src/app/services/coffee-varieties.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {ICoffeeVarieties} from '../interfaces/cofee-varieties';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class CoffeeVarietiesService {
+	apiUrl =
+		'https://gist.githubusercontent.com/josejbocanegra/e9d24db370ce95b75555f7d1f8691805/raw/8a26ac2bca4183dc88545e14c45851d698911358/202212_MISW4104_Grupo3.json';
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	getCoffeeVarieties(): Observable<ICoffeeVarieties[]> {
+		return this.http.get<ICoffeeVarieties[]>(this.apiUrl);
+	}
 }
