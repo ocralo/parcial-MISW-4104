2c0a2d98b0c2b15e30842fcedaecd8d16105b103
ocralo <ocralo@gmail.com>
2022-11-04 01:13:43 -0500

    feat: add service in component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ICoffeeVarieties} from 'src/app/interfaces/cofee-varieties';
+import {CoffeeVarietiesService} from 'src/app/services/coffee-varieties.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+	coffeeVarieties: ICoffeeVarieties[] = [];
 
-  constructor() { }
+	constructor(private coffeeVarietiesService: CoffeeVarietiesService) {}
 
-  ngOnInit(): void {
-  }
+	getCoffeeVarieties(): void {
+		this.coffeeVarietiesService
+			.getCoffeeVarieties()
+			.subscribe(this.setCoffeeVarieties.bind(this));
+	}
 
+	setCoffeeVarieties(museums: ICoffeeVarieties[]) {
+		this.coffeeVarieties = museums;
+	}
+
+	ngOnInit(): void {
+		this.getCoffeeVarieties();
+	}
 }
