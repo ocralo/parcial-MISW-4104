099bd8c5d5fdd0c19f8230776951a58a69c6bb6f
ocralo <ocralo@gmail.com>
2022-11-04 01:13:50 -0500

    feat: add tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,23 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
+import coffeeVarieties from '../../__mocks__/coffee-varieties.json';
+import {ICoffeeVarieties} from 'src/app/interfaces/cofee-varieties';
+import {CoffeeVarietiesService} from 'src/app/services/coffee-varieties.service';
+import {of} from 'rxjs';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+	let component: HomeComponent;
+	let fixture: ComponentFixture<HomeComponent>;
+	const expectedCoffeeVarieties: ICoffeeVarieties[] = coffeeVarieties;
+	let fakeCoffeeVarietiesService: CoffeeVarietiesService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		fakeCoffeeVarietiesService = jasmine.createSpyObj<CoffeeVarietiesService>(
+			'CoffeeVarietiesService',
+			{
+				getCoffeeVarieties: of(expectedCoffeeVarieties),
+			}
+		);
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		await TestBed.configureTestingModule({
+			declarations: [HomeComponent],
+			providers: [
+				{provide: CoffeeVarietiesService, useValue: fakeCoffeeVarietiesService},
+			],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(HomeComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('getCoffeeVarieties', () => {
+		it('should be call setMuseums by service', () => {
+			const spySetMuseums = spyOn(component, 'setCoffeeVarieties');
+			component.getCoffeeVarieties();
+			fixture.detectChanges();
+			expect(spySetMuseums).toHaveBeenCalled();
+		});
+	});
+
+	describe('setMuseums', () => {
+		it('should be change museums', () => {
+			const otherMuseum = [expectedCoffeeVarieties[0]];
+			component.setCoffeeVarieties(otherMuseum);
+			fixture.detectChanges();
+			expect(component.coffeeVarieties).toEqual(otherMuseum);
+		});
+	});
 });
