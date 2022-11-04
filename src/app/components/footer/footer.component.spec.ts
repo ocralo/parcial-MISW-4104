0a9c0b4dfcbfe624d5e2b647ff17f9bd485bbc74
ocralo <ocralo@gmail.com>
2022-11-04 00:31:47 -0500

    feat: add new test

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -1,23 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
-import { FooterComponent } from './footer.component';
+import {FooterComponent} from './footer.component';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+	let component: FooterComponent;
+	let fixture: ComponentFixture<FooterComponent>;
+	let element: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [FooterComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(FooterComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('Footer text', () => {
+		beforeEach(() => {
+			element = fixture.debugElement.query(By.css('.footer-text'));
+		});
+
+		it('should be containt El aroma mágico', () => {
+			expect(element.nativeElement.textContent.trim()).toBe(
+				'Contact us: +57 3102105253 - info@elaromamagico.com - @elaromamagico'
+			);
+		});
+	});
 });
