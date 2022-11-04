2bcde368f435955d737592a4449b8e09bd4783ec
ocralo <ocralo@gmail.com>
2022-11-04 01:13:39 -0500

    feat: add httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeroImageComponent } from './components/hero-image/hero-image.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {HeroImageComponent} from './components/hero-image/hero-image.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    HeroImageComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		NavbarComponent,
+		HeroImageComponent,
+		FooterComponent,
+	],
+	imports: [HttpClientModule, BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
